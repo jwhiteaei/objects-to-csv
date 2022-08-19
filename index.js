2f811c944d42f4c7d93c38cae27ae16a48b967e0
jwhiteaei <42450757+jwhiteaei@users.noreply.github.com>
2022-08-19 09:26:24 -0500

    Update index.js
    Removed the toDisk Method, and the fs import, for browser use in React

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const csv = require('async-csv');
-const fs = require('fs');
 
 /**
  * Converts an array of objects into a CSV file.
@@ -23,65 +22,6 @@ class ObjectsToCsv {
     }
 
     this.data = objectArray;
-  }
-
-  /**
-   * Saves the CSV file to the specified file.
-   * @param {string} filename - The path and filename of the new CSV file.
-   * @param {object} options - The options for writing to disk.
-   * @param {boolean} [options.append] - Whether to append to file. Default is overwrite (false).
-   * @param {boolean} [options.bom] - Append the BOM mark so that Excel shows
-   * @param {boolean} [options.allColumns] - Whether to check all items for column names or only the first.  Default is the first.
-   * Unicode correctly.
-   */
-  async toDisk(filename, options) {
-    if (!filename) {
-      throw new Error('Empty filename when trying to write to disk.');
-    }
-
-    let addHeader = false;
-
-    // If the file didn't exist yet or is empty, add the column headers
-    // as the first line of the file. Do not add it when we are appending
-    // to an existing file.
-    const fileNotExists = !fs.existsSync(filename) || fs.statSync(filename).size === 0;
-    if (fileNotExists || !options || !options.append) {
-      addHeader = true;
-    }
-
-    const allColumns = options && options.allColumns
-      ? options.allColumns
-      : false;
-
-    let data = await this.toString(addHeader, allColumns);
-    // Append the BOM mark if requested at the beginning of the file, otherwise
-    // Excel won't show Unicode correctly. The actual BOM mark will be EF BB BF,
-    // see https://stackoverflow.com/a/27975629/6269864 for details.
-    if (options && options.bom && fileNotExists) {
-      data = '\ufeff' + data;
-    }
-
-    if (options && options.append) {
-      return new Promise((resolve, reject) => {
-        fs.appendFile(filename, data, 'utf8', (error) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(data);
-          }
-        });
-      });
-    } else {
-      return new Promise((resolve, reject) => {
-        fs.writeFile(filename, data, 'utf8', (error) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(data);
-          }
-        });
-      });
-    }
   }
 
   /**
